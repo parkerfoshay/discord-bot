ee61764ece7b46fc8eac69cd408fdc1b1928b14a
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-05-10 15:21:55 -0700

    moe

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, MessageAttachment} = require("discord.js");
+const {Client, MessageAttachment, MessageEmbed} = require("discord.js");
 const config = require("./config.json");
 
 const client = new Client();
@@ -43,6 +43,7 @@ client.on("message", function (message) {
   }
 });
 
+
 client.on('message', message => {
     // If the message is '!rip'
     if (message.content === '!nathan') {
@@ -51,6 +52,25 @@ client.on('message', message => {
       // Send the attachment in the message channel
       message.channel.send(`The beautiful beast`, attachment);
     }
+  });
+
+  client.on('message', message => {
+    if (message.mentions.has(client.user.id)) {
+         // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+    // Set the title of the field
+    .setTitle('Just stop, please')
+    // Set the color of the embed
+    .setColor(0xff0000)
+    // Set the main content of the embed
+    .setDescription('I am busy right now')
+    .setImage(`https://i.pinimg.com/236x/f1/4c/01/f14c01d8d0bd7b1a831fed561238a99c.jpg`)
+  // Send the embed to the same channel as the message
+  message.channel.send(embed);
+    };
+    
   });
 
 client.login(config.BOT_TOKEN);
